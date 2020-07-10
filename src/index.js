329e2d27c0b6fdd849e28f2b56831427f3b58599
Injae Lee <github email address>
2020-07-10 09:45:21 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -5,28 +5,7 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Counter />
-      </div>
-    );
-  }
-}
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-    );
-  }
-}
-
-class Counter extends Component {
   state = { count: 0 };
 
   increment = () => {
@@ -37,19 +16,42 @@ class Counter extends Component {
     this.setState({ count: this.state.count - 1 });
   };
 
+  render() {
+    return (
+      <div className="App">
+        <Header count={this.state.count} />
+        <Counter increment={this.increment} decrement={this.decrement} count={this.state.count}/>
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+
   renderDescription = () => {
-    const remainder = this.state.count % 5;
+    const remainder = this.props.count % 5;
     const upToNext = 5 - remainder;
-    return `The current count is less than ${this.state.count + upToNext}`;
+    return `The current count is less than ${this.props.count + upToNext}`;
   };
 
   render() {
     return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>{this.renderDescription()}</h1>
+      </header>
+    );
+  }
+}
+
+class Counter extends Component {
+
+  render() {
+    return (
       <div className="Counter">
-        <h1>{this.state.count}</h1>
-        <button onClick={this.decrement}> - </button>
-        <button onClick={this.increment}> + </button>
-        <h3>{this.renderDescription()}</h3>
+        <h1>{this.props.count}</h1>
+        <button onClick={this.props.decrement}> - </button>
+        <button onClick={this.props.increment}> + </button>
       </div>
     );
   }
